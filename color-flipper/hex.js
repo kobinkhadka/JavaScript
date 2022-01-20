3927e7d0d4eb7806cea752c2d089a977046147e3
kobinjung <kobinkhadka@gmail.com>
2022-01-20 14:03:39 -0600

    color flipper is Done

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -1,1 +1,30 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn   = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+
+btn.addEventListener("click", function() {
+    
+    let hexNumber = "#";
+    
+    for(let i = 0; i < 6; i++){
+        let randomNumber = getRandomNumber()
+        hexNumber += hex[randomNumber]; 
+    }
+
+    document.body.style.backgroundColor = hexNumber;
+
+    color.textContent = hexNumber;
+
+    console.log(hexNumber)
+
+
+})
+
+function getRandomNumber() { 
+
+    return Math.floor(Math.random() * hex.length)
+}
+
+
